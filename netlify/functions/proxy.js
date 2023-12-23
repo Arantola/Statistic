97e1ffca1b58c281836e0f21b4499ab67d2f9654
Arantola <Arantola@mail.ru>
2023-12-23 15:20:31 +0300

    1

--- netlify/functions/proxy.js
+++ netlify/functions/proxy.js
@@ -1,5 +1,6 @@
 // netlify/functions/proxy.js
-const fetch = require("node-fetch");
+import fetch from "node-fetch";
+
 const URL =
   "https://gipsy-partners.com/api/sys/analytics.json-pretty?id=1-c89b95bac18d8810ea68d56a32db1aca&item=comp&user=81&phase=3&from=2023-11-01&to=2023-12-01";
 exports.handler = async function (event, context) {
